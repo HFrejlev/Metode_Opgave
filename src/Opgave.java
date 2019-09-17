7369e84bd8cbacee8bfc7491561667fc9828b7ce
HFrejlev <henrikfrejlev@hotmail.com>
2019-09-17 10:29:13 +0200

    Initial commit

---
+++ src/Opgave.java
@@ -0,0 +1,18 @@
+public class Opgave {
+
+    public static void main(String[] args) {
+        //Testcase hvor sidste værdi er midterst
+        //Input: a = 6, b= 10, c = 8
+        //Forventet output: 8
+
+        //Testcase hvor første værdi er midterst
+        //Input: a = 3, b= 7, c = 2
+        //Forventet output: 3
+
+        //Testcase hvor midterste værdi er midterst
+        //Input: a = 10, b= 25, c = 50
+        //Forventet output: 25
+    }
+
+
+}
